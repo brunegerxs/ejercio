fe82f96c57adf480edc944023edf21ad37388677
brunegerxs <bbaeznebiolo@escuelasproa.edu.ar>
2026-03-20 16:05:17 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,3 +1,7 @@
 import pandas as pd
 df=pd.read_csv('StudentsPerformance.csv')
 print(df['gender'])
+print(df['gender'].head())
+print(df['lunch'].head(12))
+print(df['gender'].tail(12))
+print(df['lunch'].tail(12))
