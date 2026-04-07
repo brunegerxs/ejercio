2859e50a95159e1095e9ff3991ed0e9c9e6eeb06
brunegerxs <bbaeznebiolo@escuelasproa.edu.ar>
2026-04-07 14:37:15 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("StudentsPerformance.csv")
+
+print(df["math score"].sum())
+print(df["math score"].min())
+print(df["math score"].max())
+print(df["math score"].count())
+#print(df.head())
